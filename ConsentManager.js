18c91562a4c2428b7c75ac0ed9c919c9411c1234
Marcel Munevar <marcel.munevar@fujifilm.com>
2025-11-12 10:56:44 -0500

    seperated files

---
+++ ConsentManager.js
@@ -0,0 +1,39 @@
+class ConsentManager {
+    enableTargeting() {
+        event.preventDefault();
+        OneTrust.UpdateConsent("Category", "C0004:1").then(() => {
+            this.hideElements();
+        }).catch(error => {
+            console.error('Error updating consent:', error);
+        });
+    }
+    isC0004Active() {
+        if (typeof OnetrustActiveGroups !== 'undefined') {
+            return OnetrustActiveGroups.includes('C0004');
+        }
+        return false;
+    }
+    hideElements() {
+        var videoElem = document.getElementsByClassName("vimeo-video");
+        var warningElem = document.getElementsByClassName("video-consent-warning");
+        if (this.isC0004Active()) {
+            for (let i = 0; i < warningElem.length; i++) {
+                warningElem[i].style.display = "none";
+            }
+            for (let i = 0; i < videoElem.length; i++) {
+                videoElem[i].style.display = "block";
+            }
+        } else {
+            for (let i = 0; i < warningElem.length; i++) {
+                warningElem[i].style.display = "block";
+            }
+            for (let i = 0; i < videoElem.length; i++) {
+                videoElem[i].style.display = "none";
+            }
+        }
+    }
+}
+var consentManager = new ConsentManager();
+document.addEventListener('DOMContentLoaded', function() {
+    consentManager.hideElements();
+});
